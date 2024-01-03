fe9d423e19f731794f52ef418e35e877359ec5c1
David Wakeham <cursedhorses@gmail.com>
2024-01-02 16:38:56 -0800

    Update firefly1.js

--- assets/fireflies_data/firefly1.js
+++ assets/fireflies_data/firefly1.js
@@ -104,7 +104,7 @@ function draw() {
     text("Coherence: " + roundPC, 10, 50);
 
     if (mouseIsPressed == true) {
-        numFireflies = 1 + round(mouseY);
+        numFireflies = 1 + mouseY;
         K = 0.05*mouseX/width;
     }
 }
